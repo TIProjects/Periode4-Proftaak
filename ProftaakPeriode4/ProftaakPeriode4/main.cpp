f178ab41c1d8a248c52d226787c46b4b55c2e415
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2017-05-01 10:56:20 +0200

    Fixed it

---
+++ ProftaakPeriode4/ProftaakPeriode4/main.cpp
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+#include "main.h"
+#include "iostream"
+
+=======
+>>>>>>> c09da0191b9424f5c30fa2cc4af424414086e7af
+int main() {
+	std::cout << "Test" << std::endl;
+	getchar();
+	return 0;
+}
