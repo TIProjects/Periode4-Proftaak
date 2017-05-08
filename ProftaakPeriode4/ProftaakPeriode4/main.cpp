4c6fd62b97a425f276ea50bcfce8f7f6bd07af84
HP_FOLIO9470M\gijsb <gijs.brian.joost@hotmail.com>
2017-05-08 10:03:42 +0200

    Idle is being deleted?

--- ProftaakPeriode4/ProftaakPeriode4/main.cpp
+++ ProftaakPeriode4/ProftaakPeriode4/main.cpp
@@ -3,14 +3,12 @@
 #include "View.h"
 #include "Score.h"
 #include "ScoreBoard.h"
-<<<<<<< HEAD
 #include <GL\freeglut.h>
 #include "Idle.h"
 #include "Lanes.h"
 
 void Test()
 {
-
 	Lanes lanes = Lanes(3);
 }
 
@@ -29,12 +27,3 @@ int main(int argc, char* argv[]) {
 	glutMainLoop();
 	return 0;
 }
-=======
-
-int main() {
-    Model model = Model();
-    View view = View(&model);
-
-    return 0;
-}
->>>>>>> 3ca763f6fc9e697643f0caa46cd37b4d07e503ca
