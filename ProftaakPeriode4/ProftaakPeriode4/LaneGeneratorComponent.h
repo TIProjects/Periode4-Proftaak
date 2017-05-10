af79bc6ab306a3b96bc8318b62e056595afc4398
Bart Machielsen <bartmachielsen@gmail.com>
2017-05-10 15:24:23 +0200

    Added documentation

--- ProftaakPeriode4/ProftaakPeriode4/LaneGeneratorComponent.h
+++ ProftaakPeriode4/ProftaakPeriode4/LaneGeneratorComponent.h
@@ -2,21 +2,49 @@
 #include "DrawComponent.h"
 #include "Mesh.h"
 
+/**
+ * \brief Class for generating and showing lanes
+ */
 class LaneGeneratorComponent :
 	public DrawComponent
 {
 public:
+	/**
+	 * \brief Constructor for creating a LaneGaneratorComponent
+	 * \param laneAmount The amount of lanes to show
+	 * \param mesh The mesh that will be drawn on the lanes
+	 */
 	LaneGeneratorComponent(int laneAmount, Mesh * mesh);
+
+	/**
+	 * \brief Destructor
+	 * todo implement
+	 */
 	~LaneGeneratorComponent();
+	
+	/**
+	 * \brief Draw the lanes
+	 */
 	void Draw() override;
+
+	/**
+	 * \brief Update the laneLogics (move the lanes)
+	 * \param deltaTime The difference between the updates
+	 */
 	void Update(float deltaTime) override;
 
 private:
+	// the mesh to show todo multiple meshes showed randomly
 	Mesh * _mesh;
+	// the amount of lanes
 	int _laneAmount;
+	// the amount already moved (automaticly lowered)
 	float _lengthMoved;
-	float _speed = 7.5f; // todo test accurate speed
+	// the movement speed
+	float _speed = 7.5f;  // todo test accurate speed
+	// the space between the lanes
 	float _spaceBetween = 1.0f;
-	int _lengthAmount = 20;
+	// the amount of lanes (in length) 
+	int _lengthAmount = 20; // todo refactor to lengthamount as distance and not amount
 };
 
