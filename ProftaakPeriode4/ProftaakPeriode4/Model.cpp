a623f6ae239e19bbb5716ac724db89db3f7ab6de
DESKTOP-S8PIJ7G\Joey <jvanhoek@hotmail.com>
2017-05-31 14:46:59 +0200

    New music file added; the changes are glorious

--- ProftaakPeriode4/ProftaakPeriode4/Model.cpp
+++ ProftaakPeriode4/ProftaakPeriode4/Model.cpp
@@ -80,7 +80,7 @@ void Model::InitSound()
 
 	//example on how to start sound(s)
 	Sound * backgroundMusic = new Sound("Assets/background.wav", true);
-	//backgroundMusic->Play();
+	backgroundMusic->Play();
 
 	//place this where the program closes
 	//BASS_Free();
